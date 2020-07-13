235c9d47eef8b389a6b36ee38ab2c57e89e086d7
anu-1806 <anushrimaurya@gmail.com>
2020-07-13 16:57:09 +0530

    starting commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: null,
+      ),
+    );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
