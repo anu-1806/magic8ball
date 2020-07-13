612dcad866fd37bbb2b720bfa8066ecadadc0923
anu-1806 <anushrimaurya@gmail.com>
2020-07-13 19:44:48 +0530

    set state widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('Ask me anything')),
+          ),
+          backgroundColor: Colors.blue[100],
+          body: MagicBall(),
+        ),
       ),
     );
 
@@ -12,8 +19,22 @@ class MagicBall extends StatefulWidget {
 }
 
 class _MagicBallState extends State<MagicBall> {
+  int ballNumber;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ],
+    );
   }
 }
